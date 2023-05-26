1904091c8f607a262413f02738217430e7c6e270
Ko Ko Maung <tonnydustin@gmail.com>
2023-05-26 20:49:58 +0630

    add network layer

---
+++ lib/constants/api_constant.dart
@@ -0,0 +1,17 @@
+///MOVIE BASE URL
+const kMovieBaseURL = 'https://api.themoviedb.org/3';
+
+///End Point
+const kSummaryEndPoint = '/summary';
+const kGetNowPlayingMoviesEndPoint = '/movie/now_playing';
+const kGetMovieDetailsEndPoint = '/movie/{$kPathParameterMovieID}';
+const kGetPopularMoviesEndPoint = '/movie/top_rated';
+
+///API KEY
+const kApiKey= '6ba57a8fa213613268b61de49d70fffe';
+
+///Query Params
+const kQueryParamsApiKey = 'api_key';
+
+///Path Parameter
+const kPathParameterMovieID = 'movie_id';
