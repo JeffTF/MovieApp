67b01dce6842f5681f213a59e732722c0d2097dc
Ko Ko Maung <tonnydustin@gmail.com>
2023-05-26 21:03:20 +0630

    add network layer

--- lib/data/vos/genres_vos/genres_vo.g.dart
+++ lib/data/vos/genres_vos/genres_vo.g.dart
@@ -7,11 +7,11 @@ part of 'genres_vo.dart';
 // **************************************************************************
 
 GenresVO _$GenresVOFromJson(Map<String, dynamic> json) => GenresVO(
-  json['id'] as int?,
-  json['name'] as String?,
-);
+      json['id'] as int?,
+      json['name'] as String?,
+    );
 
 Map<String, dynamic> _$GenresVOToJson(GenresVO instance) => <String, dynamic>{
-  'id': instance.id,
-  'name': instance.name,
-};
+      'id': instance.id,
+      'name': instance.name,
+    };
